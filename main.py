f71579a03df4ab1fd348d262331ae7770d449956
hero61921 <hero61921@gmail.com>
2026-05-28 04:11:24 +0530

    main.py

--- main.py
+++ main.py
@@ -21,7 +21,6 @@ import requests
 import aiohttp
 import httpx
 import telebot
-import telegram
 from urllib.parse import urlparse
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
